57ced22673e070b894518b5f1519fd5f25bd6109
Joe Hart <joehart42@outlook.com>
2021-05-15 10:22:13 +0100

    test husky

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,6 +8,7 @@ type Props = {
 
 export default function Button({ onClick, children }: Props) {
   
+  
   return (
     <button type="button" onClick={onClick} className={styles.root}>
       {children}

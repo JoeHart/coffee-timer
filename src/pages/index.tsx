8819a1fb7cc09e923246a6207fd4d0f7199027ee
Joe Hart <joehart42@outlook.com>
2021-05-15 10:38:22 +0100

    visual counter of boops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,19 @@
+import * as React from 'react'
 import Button from '../components/Button'
 
 export default function Homepage() {
+  const [boops, setBoops] = React.useState(0)
   const onBoop = () => {
     // eslint-disable-next-line no-console
     console.log(`Boop`)
+    setBoops((b) => b + 1)
   }
-
   return (
     <div>
       <h1>Next TS Starter</h1>
       <p>Go on and build something ya filthy animal.</p>
       <Button onClick={onBoop}>Boop</Button>
+      <p>Boop-o-meter: {boops}</p>
     </div>
   )
 }
